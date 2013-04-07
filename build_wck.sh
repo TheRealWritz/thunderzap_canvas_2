2475c1a59c37ff651e83c40a6912b14448ff2afe
petito.basni <petito.basni@gmail.com>
2013-04-07 16:06:55 +0200

    build script

--- build_wck.sh
+++ build_wck.sh
@@ -1,2 +1,1 @@
 make TARGET_PRODUCT=s9081 MTK_ROOT_CUSTOM=mediatek/custom CROSS_COMPILE=/home/nightelf/dvpt/android/prebuilts/gcc-linaro-4.6-2012.11/bin/arm-eabi-
-#./repack.pl -boot arch/arm/boot/zImage ramdisk boot.img
--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
-make TARGET_PRODUCT=s9081 MTK_ROOT_CUSTOM=mediatek/custom CROSS_COMPILE=/home/nightelf/dvpt/android/my-toolchain-arm-eabi-gcc-linaro-4.6/arm-eabi-4.6/bin/arm-eabi- clean mrproper
+make TARGET_PRODUCT=s9081 MTK_ROOT_CUSTOM=mediatek/custom CROSS_COMPILE=/home/nightelf/dvpt/android/prebuilts/gcc-linaro-4.6-2012.11/bin/arm-eabi- clean mrproper
 rm -rf boot.img
 rm -rf kernelflasher.zip
